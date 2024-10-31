3232e74d1a1d1d1062d38a817fa6e9e68a1e0488
pircoderx <pirunovas24@gmail.com>
2024-10-31 15:35:51 +0200

    L1X1

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
@@ -1,5 +1,32 @@
 package com.techyourchance.unittestingfundamentals.exercise1;
 
-public class NegativeNumberValidatorTest {
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
+import static org.hamcrest;
+
+public class NegativeNumberValidatorTest {
+    NegativeNumberValidator nnv;
+
+    @Before
+    public void setup() {
+        nnv = new NegativeNumberValidator();
+    }
+
+    @Test
+    public void test1() {
+        Assert.assertThat(nnv.isNegative(-100), CoreMatchers.is(true));
+    }
+
+    @Test
+    public void test2() {
+        Assert.assertThat(nnv.isNegative(42), CoreMatchers.is(false));
+    }
+
+    @Test
+    public void test3() {
+        Assert.assertThat(nnv.isNegative(0), CoreMatchers.is(false));
+    }
 }
