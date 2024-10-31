7cdf279ae426ac6e1719ac383aa58a2c17ddda27
pircoderx <pirunovas24@gmail.com>
2024-10-31 16:08:08 +0200

    L1X1 fix

--- unit_testing_fundamentals/src/main/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidator.java
+++ unit_testing_fundamentals/src/main/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidator.java
@@ -4,6 +4,6 @@ public class NegativeNumberValidator {
 
     public boolean isNegative(int number) {
         // the bug is that 0 will be reported as negative while it's not
-        return number <= 0;
+        return number < 0;
     }
 }
